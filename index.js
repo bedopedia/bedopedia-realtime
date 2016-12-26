1b50bf26ef9495b3f75e4b92afbe67daad7839a3
Fahmy <a7med.fahmy94@gmail.com>
2016-12-26 13:47:48 +0200

    allow cross origin

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ var express = require('express'),
     io = io.listen(server);
 server.listen(8080);
 app.use(bodyParser.json());
-
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "X-Requested-With");
+        res.header("Access-Control-Allow-Headers", "Content-Type");
+        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+        next();
+    });
 console.log('listening on 8080');
 
 // notifications
