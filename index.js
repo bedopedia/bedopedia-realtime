273e3ae1f781587e8a70c74e9eb734d9a2c1777f
Fahmy <a7med.fahmy94@gmail.com>
2017-03-15 13:19:39 +0200

    schools dictionary

--- index.js
+++ index.js
@@ -5,40 +5,46 @@ var express = require('express'),
     app = express(),
     server = http.createServer(app),
     io = io.listen(server);
+
+require('./schools_dict')(app);
+
 server.listen(8080);
+console.log('listening on 8080');
+
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
-    });
-console.log('listening on 8080');
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
+
+
 
 // notifications
 // require('./notifications/notifications.js')(io);
 io.on('connection', function(socket) {
-  console.log('connected...');
-  socket.on('message', function(from, msg) {
-    console.log('recieved message from',from, 'msg', JSON.stringify(msg));
-    console.log('broadcasting message');
-    console.log('payload is', msg);
-    io.sockets.emit('broadcast', {
-      payload: msg,
-      source: from
-    });
-    console.log('broadcast complete');
+console.log('connected...');
+socket.on('message', function(from, msg) {
+  console.log('recieved message from',from, 'msg', JSON.stringify(msg));
+  console.log('broadcasting message');
+  console.log('payload is', msg);
+  io.sockets.emit('broadcast', {
+    payload: msg,
+    source: from
   });
+  console.log('broadcast complete');
+});
 });
 
 app.post('/realtime',function(request, response){
-  var body = request.body;
-  console.log('=================');
-  console.log(body);
-  console.log('=================');
-  if (body.event) {
-    io.sockets.emit(body.event,body.payload);
-  }
-  response.json({status: 'okay'})
+var body = request.body;
+console.log('=================');
+console.log(body);
+console.log('=================');
+if (body.event) {
+  io.sockets.emit(body.event,body.payload);
+}
+response.json({status: 'okay'})
 })
---
+++ schools_dict.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var schools = JSON.parse(fs.readFileSync('schools.json', 'utf8'));
+
+module.exports = function(app){
+  app.get('/schools_dict/:name', function(request, response){
+    var url = schools[request.params.name]
+    if (url != undefined) {
+      response.status(200).json({url: url})
+    } else {
+      response.status(404).json({error: "school not found"})
+    }
+  })
+}
