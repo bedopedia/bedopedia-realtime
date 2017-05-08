b5002b00b15110ef7e86cbfad5bb0abb1978c3f0
Mohamed Khaled <mohamed.khaled1110@gmail.com>
2017-05-08 16:01:34 +0200

    send to tokens

--- notifications/methods.js
+++ notifications/methods.js
@@ -1,4 +1,9 @@
 var MongoDB = require('../mongodb/methods')
+var FCM = require('fcm-node');
+const SERVERKEY = 'AAAArO2sV0k:APA91bHGhvw6EX-42rDXvj64OS3X-6e_SKtQ95p7NSTHVGwTMj4vEUxMXbzJfFYpSvhK_eaJPxG8zyN0UUpG3MbjbhXeRwVTUgI1KGtioggrT8o9VfD2XBgzW6o0MBNhahbJ7zRadbiZ';
+var fcm = new FCM(SERVERKEY);
+
+var message;
 // user should have school & id
 // data should have event & payload
 // event can be notification or message
@@ -10,6 +15,15 @@ module.exports.notify = function(user, data, io) {
         if (socket) {
           console.log(data);
           socket.emit(data.event, data.payload)
+        }
+      })
+      user.tokens.forEach((tokenID)=>{
+        if (tokenID) {
+          console.log(data);
+          message = {
+            to: tokenID,
+            data: data
+          };
         }
       })
       // push to tokens
